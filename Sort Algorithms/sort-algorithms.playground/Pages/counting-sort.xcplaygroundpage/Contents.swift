fe08e162dd452c4b30dfb1ae982c5f3baa61c3f8
KevinAo22 <kevinao.zhang@gmail.com>
2018-01-27 02:43:00 +0800

    Update

--- Sort Algorithms/sort-algorithms.playground/Pages/counting-sort.xcplaygroundpage/Contents.swift
+++ Sort Algorithms/sort-algorithms.playground/Pages/counting-sort.xcplaygroundpage/Contents.swift
@@ -3,9 +3,15 @@ import Foundation
 var numsArray = Int.randomArray(range: Range<Int>(1..<100), count: 10)
 let numsArraySorted = numsArray.sorted()
 
+// 计数排序
+// 最差时间复杂度 ---- O(n)
+// 最优时间复杂度 ---- O(n)
+// 平均时间复杂度 ---- O(n)
+// 辅助空间复杂度 ---- O(n)
+// 稳定性 ----------- 不稳定
+
 func countingSort(_ array: inout [Int]) {
-    // Step 1
-    // Create an array to store the count of each element
+    // 创建计数数组，保存数组数据的出现次数。
     let maxElement = array.max() ?? 0
     
     var countArray = [Int](repeating: 0, count: Int(maxElement + 1))
@@ -13,15 +19,13 @@ func countingSort(_ array: inout [Int]) {
         countArray[element] += 1
     }
     
-    // Step 2
-    // Set each value to be the sum of the previous two values
+    // 每一个元素等于之前的元素的和。
     for index in 1 ..< countArray.count {
         let sum = countArray[index] + countArray[index - 1]
         countArray[index] = sum
     }
     
-    // Step 3
-    // Place the element in the final array as per the number of elements before it
+    // 还原原数组
     for element in array {
         countArray[element] -= 1
         array[countArray[element]] = element
